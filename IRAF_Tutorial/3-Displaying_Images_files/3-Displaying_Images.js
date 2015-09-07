3ccee669a29dd931c48230c64be848490dd85b77
Josh Walawender <jmwalawender@gmail.com>
2015-09-07 08:33:37 -1000

    Copy in old IRAF Tutorial page (iWeb version).  Put new, unfinished md version in _drafts.

---
+++ IRAF_Tutorial/3-Displaying_Images_files/3-Displaying_Images.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120703
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('3-Displaying_Images_files/3-Displaying_ImagesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ _drafts/4-Working_with_the_CL.md
@@ -0,0 +1,4 @@
+---
+title: IRAF Tutorial
+layout: default
+---
