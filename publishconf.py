8b52ddb72bc252edb7fda04f34b8430e4ce0cb7a
Josh Walawender <jmwalawender@gmail.com>
2020-05-24 12:02:05 -1000

    Try new quickstart config

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://twilightlandscapes.com'
+SITEURL = 'http://joshwalawender.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
