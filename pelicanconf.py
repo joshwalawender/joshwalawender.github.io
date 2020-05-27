1e979f684d7e1e7620b833d90df2895a12523118
Josh Walawender <jmwalawender@gmail.com>
2020-05-27 11:55:27 -1000

    Reorg using articles dir

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,8 +34,6 @@ DEFAULT_PAGINATION = 5
 HEADER_COVER = 'images/6D-7894.jpg'
 GITHUB_URL = 'http://github.com/joshwalawender'
 
-# STATIC_PATHS = 'content/pages/IRAFtutorial'
-
 # Notes
 # pelican content -o output -s pelicanconf.py -t pelican-clean-blog -r --listen
 # ghp-import -m "Generate Pelican site" --no-jekyll -b master output
